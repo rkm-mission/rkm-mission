45cb6b654b7f9dd6cee397202ee630598b2d1c45
rkm-mission <rkm.mission@gmail.com>
2014-12-30 22:29:28 +0530

    festival option update

--- app/src/main/java/com/example/ramkrishnamathandmission/Festival.java
+++ app/src/main/java/com/example/ramkrishnamathandmission/Festival.java
@@ -9,6 +9,7 @@ import android.view.Window;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ import java.util.List;
 public class Festival extends Activity {
     WebView webView;
 
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -103,6 +105,34 @@ public class Festival extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
+    }
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_branch_map_option);
+        final Button button, button2;
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Festival.this, UlsoorFestival.class);
+                startActivity(intent);
+            }
+
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Festival.this, BullTempleFestival.class);
+                startActivity(intent);
+            }
+
+        });
     }
 
 }
--- app/src/main/java/com/example/ramkrishnamathandmission/MainActivity.java
+++ app/src/main/java/com/example/ramkrishnamathandmission/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Window;
 
 
 public class MainActivity extends Activity
