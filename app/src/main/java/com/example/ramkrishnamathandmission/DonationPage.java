f0f159bc5a50224869d0114032ec26907ac3672b
Diganta <rkm.mission@gmail.com>
2014-12-30 19:24:36 +0530

    look and feel enhancements

--- app/src/main/java/com/example/ramkrishnamathandmission/DonationPage.java
+++ app/src/main/java/com/example/ramkrishnamathandmission/DonationPage.java
@@ -7,16 +7,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.webkit.WebView;
 import android.widget.RadioButton;
 
 public class DonationPage extends Activity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_donation_page);
 
+        webView = (WebView) findViewById(R.id.donation_web);
+        //Load emblem.html from local storage.
+        webView.loadUrl("file:///android_asset/donate.html");
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 }
